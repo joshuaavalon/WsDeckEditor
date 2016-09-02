420539dddfc68ebde7b538eeee21e85a438a2821
Joshua <joshuaavalon@gmail.com>
2016-09-02 17:26:57 +0800

    Add Color bar

--- app/src/main/java/com/joshuaavalon/wsdeckeditor/model/DeckUtils.java
+++ app/src/main/java/com/joshuaavalon/wsdeckeditor/model/DeckUtils.java
@@ -6,6 +6,11 @@ import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multiset;
+
 public class DeckUtils {
 
     private static final int DECK_LIMIT = 50;
@@ -26,5 +31,23 @@ public class DeckUtils {
 
     public static int getCount(@NonNull final Deck deck) {
         return deck.getList().size();
+    }
+
+
+    public static int getCount(@NonNull final Deck deck, @NonNull final Card.Color color) {
+        return Iterables.size(Iterables.filter(deck.getList(), new Predicate<Card>() {
+            @Override
+            public boolean apply(Card input) {
+                return input.getColor() == color;
+            }
+        }));
+    }
+
+    public static Multiset<Card.Color> getColorCount(@NonNull final Deck deck) {
+        final Multiset<Card.Color> colorCount =  HashMultiset.create();
+        for (Multiset.Entry<Card> entry :  deck.getList().entrySet()) {
+            colorCount.add(entry.getElement().getColor(), entry.getCount());
+        }
+        return colorCount;
     }
 }
--- app/src/main/java/com/joshuaavalon/wsdeckeditor/view/ColorUtils.java
+++ app/src/main/java/com/joshuaavalon/wsdeckeditor/view/ColorUtils.java
@@ -1,12 +1,21 @@
 package com.joshuaavalon.wsdeckeditor.view;
 
-import android.graphics.drawable.GradientDrawable;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.PaintDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
 
 import com.joshuaavalon.wsdeckeditor.R;
+import com.joshuaavalon.wsdeckeditor.WsApplication;
 import com.joshuaavalon.wsdeckeditor.model.Card;
+import com.joshuaavalon.wsdeckeditor.model.Deck;
+import com.joshuaavalon.wsdeckeditor.model.DeckUtils;
 
 public class ColorUtils {
     @ColorRes
@@ -23,6 +32,10 @@ public class ColorUtils {
             default:
                 return R.color.cardYellow;
         }
+    }
+
+    public static int getColorValue(@ColorRes final int color) {
+        return ContextCompat.getColor(WsApplication.getContext(), color);
     }
 
     @DrawableRes
