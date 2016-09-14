1407e8d8be9148605655572e87bdf20db2c6de3c
Joshua <joshuaavalon@gmail.com>
2016-09-14 15:41:47 +0800

    Update Setting

--- app/src/main/java/com/joshuaavalon/wsdeckeditor/repository/PreferenceRepository.java
+++ app/src/main/java/com/joshuaavalon/wsdeckeditor/repository/PreferenceRepository.java
@@ -21,39 +21,38 @@ public class PreferenceRepository {
             WsApplication.getContext().getString(R.string.pref_pop_back);
 
     public static Card.SortOrder getSortOrder() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        return Card.SortOrder.fromInt(preferences.getInt(SORT_ORDER_KEY, Card.SortOrder.Detail.toInt()));
+        return Card.SortOrder.fromInt(getSharedPreferences()
+                .getInt(SORT_ORDER_KEY, Card.SortOrder.Detail.toInt()));
     }
 
     public static void setSortOrder(Card.SortOrder order) {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        final SharedPreferences.Editor editor = preferences.edit();
+        final SharedPreferences.Editor editor = getSharedPreferences().edit();
         editor.putInt(SORT_ORDER_KEY, order.toInt());
         editor.apply();
     }
 
     public static boolean getAutoSave() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        return preferences.getBoolean(AUTO_SAVE_KEY, true);
+        return getSharedPreferences().getBoolean(AUTO_SAVE_KEY, true);
     }
 
     public static boolean getHideNormal() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        return preferences.getBoolean(HIDE_NORMAL_KEY, true);
+        return getSharedPreferences().getBoolean(HIDE_NORMAL_KEY, true);
     }
 
     public static int getShowLimit() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        return Integer.valueOf(preferences.getString(SHOW_LIMIT_KEY, "200"));
+        return Integer.valueOf(getSharedPreferences().getString(SHOW_LIMIT_KEY,
+                WsApplication.getContext().getString(R.string.default_show_limit)));
     }
 
     public static boolean getAutoClose() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        return preferences.getBoolean(AUTO_CLOSE_KEY, true);
+        return getSharedPreferences().getBoolean(AUTO_CLOSE_KEY, true);
     }
 
     public static boolean getPopBack() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
-        return preferences.getBoolean(POP_BACK_KEY, true);
+        return getSharedPreferences().getBoolean(POP_BACK_KEY, true);
+    }
+
+    private static SharedPreferences getSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(WsApplication.getContext());
     }
 }
